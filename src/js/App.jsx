f8c5bcfaedf870d56185e29cad22d16584255c3e
JosueJMartinez <jmarti61@gmail.com>
2020-11-03 04:21:52 +0000

    started working on login form card

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,12 +1,34 @@
-import "../css/App.css";
-import NavBar from "./components/NavBar";
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from "@material-ui/core/styles";
 
-function App() {
+import NavBar from "./components/NavBar";
+import LoginFormCard from "./components/LoginFormCard";
+
+const styles = {
+	root:{
+		height:'100%'
+	},
+	grid:{
+		height:'100%'
+	}
+}
+
+function App(props) {
+	const {classes} = props;
+	
   return (
-    <div className="App">
+    <div className={classes.root}>
       <NavBar />
+			<Grid className={classes.grid} container justify="center" alignItems="center"
+			>
+				<Grid item xs={11} sm={9} md={6} lg={4} >
+					<LoginFormCard />
+				</Grid>
+			</Grid>
+			
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
